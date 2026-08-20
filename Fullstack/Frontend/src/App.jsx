23e8c7f50dadfe461521d7638bd58e12a4463461
Your Name <patelmitin712003@gmail.com>
2026-08-20 12:01:46 +0530

    sec6

--- Fullstack/Frontend/src/App.jsx
+++ Fullstack/Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Hero2 from "./components/Hero2";
 import Hero3 from "./components/Hero3";
 import Section4 from "./components/Section4";
+import Section6 from "./components/Section6";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
       <Hero2 />
       <Hero3 />
       <Section4 />
+      <Section6 />
+
     </div>
   );
 }
