0ae0f4b181a0bc80cce59286289feac96ec9b554
Your Name <patelmitin712003@gmail.com>
2026-09-09 16:14:26 +0530

    ..

--- Fullstack/Frontend/src/App.jsx
+++ Fullstack/Frontend/src/App.jsx
@@ -110,6 +110,7 @@ import WhyTechtorch from "./pages/What'sNext/CyberSecurity/WhyTechtorch.jsx";
 
 // ================= BUSINESS GROWTH =================
 import GrowWithBusinessSection from "./pages/What'sNext/BusinessGrowth/GrowwTechnology.jsx";
+import PurspectiveSection from "./pages/What'sNext/BusinessGrowth/PursoectiveSection.jsx";
 
 
 // =================================================
@@ -322,6 +323,7 @@ function BusinessGrowthPage() {
   return (
     <>
       <GrowWithBusinessSection />
+      <PurspectiveSection />  
     </>
   );
 }
